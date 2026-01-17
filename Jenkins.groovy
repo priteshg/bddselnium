d7b51764d5ec4401800ee47aaf8a2e30efc81372
Pritesh Gandhi <pg4ndhi@gmail.com>
2026-01-17 19:53:07 +0000

    Update Jenkins.groovy

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -1,14 +1,10 @@
-stage('Run Jest BDD tests') {
-  steps {
-    script {
-      withEnv(['JEST_RETRIES=0']) {
-        sh '''
-          set -e
-          node -e "console.log('JEST_RETRIES=', process.env.JEST_RETRIES)"
-          npx jest --showConfig | grep -i -E "testRunner|retryTimes" || true
-          npm test -- --coverage --runInBand --maxWorkers=50
-        '''
+script {
+          withEnv(['JEST_RETRIES=0']) {
+            sh '''
+              set -e
+              node -e "console.log('JEST_RETRIES=', process.env.JEST_RETRIES)"
+              npm test
+            '''
+          }
+        }
       }
-    }
-  }
-}
